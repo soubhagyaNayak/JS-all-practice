5830c96d7c4b6538a2d658b0ede42f599671160a
Soubhagya Ranjan Nayak <45428643+soubhagyaNayak@users.noreply.github.com>
2024-07-20 12:52:33 +0000

    string

---
+++ 04_String/first.js
@@ -0,0 +1,16 @@
+const name=new String("Soubhagya");
+//console.log(name.length);
+const email="soubhagyaranjannayak68@gmail.com";
+const uname="     srtyytyuityu     ";
+//how to write string in uppercase
+//console.log(name.toUpperCase());
+console.log(name.slice(0,3));
+console.log(name.split('a'));//spit the string one the basis of character a
+//how to find the position of index
+// console.log(name.charAt(2));
+// console.log(name.indexOf('u'));
+// console.log(uname.trim());
+// console.log(name.includes('bha'));
+ 
+// console.log(name.substring(0,4));
+// console.log(email.replace("68","789"));
